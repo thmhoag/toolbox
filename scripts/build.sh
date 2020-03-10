3ad962a7a8e97740f28f4827213ec4f189c35338
Thomas Hoag <thmhoag@gmail.com>
2020-03-09 22:41:59 -0400

    Add build script

---
+++ scripts/build.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+REPODIR="$(dirname "$SCRIPTDIR")"
+cd "$REPODIR"
+
+# always fail script if a cmd fails
+set -eo pipefail
+
+# required commands
+command -v docker >/dev/null 2>&1 || { echo "docker is required but not installed, aborting..." >&2; exit 1; }
+
+docker build . -t thmhoag/toolbox:latest
